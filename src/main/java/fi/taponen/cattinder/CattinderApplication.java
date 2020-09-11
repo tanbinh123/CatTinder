74a50896856ff662f9446fef1e567e9ae0814f45
ullataponen <ulla.taponen@gmail.com>
2020-09-11 15:21:44 +0300

    Initial setup & dependencies

---
+++ src/main/java/fi/taponen/cattinder/CattinderApplication.java
@@ -0,0 +1,13 @@
+package fi.taponen.cattinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CattinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CattinderApplication.class, args);
+	}
+
+}
