88ed312d92f31dcf57983d2d9ff8840fe27a0eb8
ullataponen <ulla.taponen@gmail.com>
2020-10-01 16:17:38 +0300

    add REST API

--- src/main/java/fi/taponen/cattinder/domain/BreedRepository.java
+++ src/main/java/fi/taponen/cattinder/domain/BreedRepository.java
@@ -3,5 +3,4 @@ package fi.taponen.cattinder.domain;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BreedRepository extends CrudRepository<Breed, Long> {
-
 }
--- src/main/java/fi/taponen/cattinder/domain/CatRepository.java
+++ src/main/java/fi/taponen/cattinder/domain/CatRepository.java
@@ -1,7 +1,10 @@
 package fi.taponen.cattinder.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CatRepository extends CrudRepository<Cat, Long> {
-
+	List<Cat> findByLocation(@Param("location") String location);
 }
